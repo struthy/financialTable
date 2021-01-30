228a1f1911137482b3c2640d3adee9b86a84b726
adam <adamselectro@gmail.com>
2021-01-30 15:27:39 +0000

    financial table start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <table>
+      <tr>
+        <th @click="sort('assetClass')">Sort by Asset Class</th>
+        <th @click="sort('price')">Sort by Price</th>
+        <th @click="sort('ticker')">Sort by Ticker</th>
+      </tr>
+
+      <tr
+        v-for="(item, i) in sortedItems"
+        :key="i + 'A'"
+        v-bind:class="item.assetClass"
+      >
+        <td>{{ item.assetClass }}</td>
+        <td :class="{ positive: item.price > 0, negative: item.price <= 0 }">
+          {{ item.price }}
+        </td>
+        <td>{{ item.ticker }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
+Array.prototype.unique = function() {
+  return this.filter(function(value, index, self) {
+    return self.indexOf(value) === index;
+  });
+};
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      items: [],
+      currentSort: "Asset Class",
+      currentSortDir: "asc",
+      isNegative: false,
+    };
+  },
+
+  created() {
+    var _this = this;
+    axios
+      .get("http://localhost:8080/sampleData.json")
+      .then((response) => {
+        _this.items = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+
+  methods: {
+    sort: function(s) {
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+      }
+      this.currentSort = s;
+    },
+  },
+  computed: {
+    sortedItems: function() {
+      return [...this.items].sort((a, b) => {
+        let modifier = 1;
+        if (this.currentSortDir === "desc") modifier = -1;
+        if (a[this.currentSort] < b[this.currentSort]) return 1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return -1 * modifier;
+        return 0;
+      });
+    },
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+tr,
+td {
+  text-align: left;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+th {
+  background-color: #333;
+  color: white;
+}
+
+th:hover {
+  cursor: pointer;
+  background-color: black;
+}
+
+tr.Macro {
+  background-color: white;
+}
+
+tr.Equities {
+  background-color: lightsteelblue;
+}
+
+tr.Credit {
+  background-color: lightseagreen;
+}
+
+td.negative {
+  color: red;
+}
+
+td.positive {
+  color: blue;
 }
 </style>
