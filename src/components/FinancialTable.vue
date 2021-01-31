0e8536e7a854b13297d51b37bd7a4db8d55a087f
adam <adamselectro@gmail.com>
2021-01-31 12:52:49 +0000

    addind dist folder

---
+++ src/components/FinancialTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "FinancialTable",
+  props: {
+    msg: String
+  },
+
+  computed: {
+    APIURL: function() {
+      return `sampleData.json`;
+    }
+  },
+
+  created() {
+    axios.get(this.APIURL).then(response => {});
+  }
+};
+</script>
